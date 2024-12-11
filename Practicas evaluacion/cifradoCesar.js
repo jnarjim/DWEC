73f5eb110642fef114f767b6c3e61f1da817b415
jnarjim <jnarjim817@g.educaand.es>
2024-12-11 12:03:47 +0100

    Actualizar ejercicios

--- Practicas evaluacion/cifradoCesar.js
+++ Practicas evaluacion/cifradoCesar.js
@@ -1,24 +1,48 @@
 let alfabetos = {
-    'castellano' : 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ',
-    'ingles' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    'castellano' : 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.split(''), // Separa las letras de los abecedarios para recorrer el texto en orden
+    'ingles' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 }
 
 function rotacionMaxima() {
+    // Alfabeto seleccionado
+    let alfabeto = document.getElementById("alfabeto").value;
 
-        let alfabeto = document.getElementById("alfabeto").value;
-
-        document.getElementById("rotacion").max = alfabetos[alfabeto].length - 1;
+    // Asigna el número de rotación máxima dependiendo del alfabeto seleccionado, español 26 e ingles 25
+    document.getElementById("rotado").max = alfabetos[alfabeto].length - 1;
 }
 
 function descifrar() {
 
-    let alfabeto = document.getElementById("alfabeto").value;
-    let alfabeto_seleccionado = alfabetos[alfabeto];
-    let rot = parseInt(document.getElementById("rotacion").value);
-    let texto = document.getElementById("texto").value;
-    let resultado = '';
+    let alfabeto = document.getElementById("alfabeto").value; // Seleccionar alfabeto
+    let alfabeto_seleccionado = alfabetos[alfabeto]; // Accede al valor del diccionario para guardar el alfabeto que se quiere usar
 
-    for (let i = 0; i < texto.length; i++) {
-        
+    let rotado = parseInt(document.getElementById("rotado").value); // Guarda el nivel de rotacion
+
+    let texto = document.getElementById("texto").value; // Guarda el texto a descifrar
+
+    let resultado = ''; // String vacío en el que estará el resultado
+
+    for (let i = 0; i < texto.length; i++) { // Recorro el texto letra a letra
+
+        let letra = texto.charAt(i); // Guardo la letra en la que me encuentro
+        let indice = alfabeto_seleccionado.indexOf(letra);
+         // Guardo el índice de la letra cifrada almacenada para actualizar el índice, si no encuentra la letra en el diccionario devuelve -1 (indexOf)
+
+        if (indice !== -1) { 
+        // Si encuentra la letra actualizo el índice, por ejemplo si Y es 26 y tiene rotacion 2, su nuevo índice sería 24 y la letra sería W (si indice no es igual a -1)
+
+            let nuevoIndice = (indice - rotado + alfabeto_seleccionado.length) % alfabeto_seleccionado.length;
+             // Calcular el nuevo índice, por ejemplo si tienes la A y la rotacion es 2, seria la Y para que no diese con un espacio vacío
+
+            resultado += alfabeto_seleccionado[nuevoIndice]; // Las letras descifradas se van añadiendo al string de resultado (que inicia vacío)
+
+        } else { 
+
+            resultado += letra; // Si no encuentra letra, por ejemplo un !, no afectaría y se queda igual
+        }
     }
+
+    // Mostrar el resultado de color rojo
+    document.getElementById("resultado").textContent = resultado;
+    document.getElementById("resultado").style.color = "red";
 }
--- Practicas evaluacion/funcionesExamen.js
+++ Practicas evaluacion/funcionesExamen.js
@@ -1,12 +1,12 @@
 function esCurioso(num) {
-    let digitos = String(num).split(''); // Convierte el numero a array de dígitos, por ejemplo 111 seria 1, 1, 1
-    let sumaCubos = 0;
+    let digitos = String(num).split(''); // Divide el numero en dígitos, por ejemplo 111 seria 1, 1, 1
+    let suma = 0;
 
-    // Sumar los cubos de los dígitos, por ejemplo seria 1 al cubo + 1 al cubo + uno al cubo
+    // Sumar los cubos de los dígitos, por ejemplo seria 1 al cubo + 1 al cubo + 1 al cubo
     for (let i = 0; i < digitos.length; i++) { 
-        sumaCubos += Math.pow(parseInt(digitos[i]), 3);
+        suma += Math.pow(parseInt(digitos[i]), 3); // Convierte los dígitos a numeros para operar, suma los numeros elevados al cubo (Math.pow)
     }
 
     // Verificar si coincide con el número original, por ejemplo, como la suma anterior da 3 y no 111, 111 no es un numero curioso
-    return sumaCubos === num; // si esto ocurre, el numero es curioso y se añadiria al array de numeros curiosos
+    return suma === num; // si esto ocurre, el numero es curioso y se añadiria al array de numeros curiosos
 }
